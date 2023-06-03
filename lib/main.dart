29f093215333ab5ef4ab2d54d1f1d4a829052c5e
Sayanth T M <128717377+Artemis963@users.noreply.github.com>
2023-06-03 22:42:38 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,79 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animated_splash/flutter_animated_splash.dart';
+
 
 void main() {
-  runApp(Duck());
+  runApp(Duckling());
 }
 
-class Duck extends StatelessWidget {
+class Duckling extends StatefulWidget {
+  @override
+  _DucklingState createState() => _DucklingState();
+}
+
+class _DucklingState extends State<Duckling>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 2500),
+    );
+
+    _animation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.decelerate,
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      highContrastDarkTheme: ThemeData.dark(),
-      highContrastTheme: ThemeData(),
-      home:Duckling() );
-  }
-}
-
-class Duckling extends StatefulWidget {
-
-
-  @override
-  State<Duckling> createState() => _DucklingState();
-}
-
-class _DucklingState extends State<Duckling> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.black54,body: Center(child: Image.asset('assets/image/ass.png',width: 300,)),);
+      home: Scaffold(
+        backgroundColor: Colors.black54,
+        body:
+            Column(mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,children: [
+          Center(
+            child: Column(
+              children: [
+                ScaleTransition(
+                  scale: _animation,
+                  child: Image.asset(
+                    'assets/image/ass.png',
+                    width: 300,
+                  ),
+                ),SizedBox(height: 80,),
+                FadeTransition(
+                  opacity: _animation,
+                  child: Text(
+                    'This is Google Fonts',
+                    style: GoogleFonts.lato(
+                      textStyle: Theme.of(context).textTheme.headline4,
+                      fontSize: 48,
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ]),
+      ),
+    );
   }
 }
