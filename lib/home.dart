25a5681b59413f8369d97067db1522d14dbe8608
sayanth <sayanth.t.m9090@gmail.com>
2023-06-04 19:44:53 +0530

    init

--- lib/home.dart
+++ lib/home.dart
@@ -9,8 +9,13 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Screen'),
+      appBar: AppBar(actions: [ Center(child: IconButton(onPressed: (){}, icon: Icon(Icons.search)))],
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomRight: Radius.circular(60),
+          ),
+        ),
+        title: Text('Artemis'),
       ),
       body: Center(
         child: Text('Welcome to the Home Screen!'),
